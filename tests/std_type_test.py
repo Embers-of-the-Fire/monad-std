c3958d18c10476331ef5f44518b91533b814e73c
Embers-of-the-Fire <stellarishs@163.com>
2024-02-04 13:59:07 +0800

    fix: change std-type test

--- tests/std_type_test.py
+++ tests/std_type_test.py
@@ -4,38 +4,32 @@ from monad_std.std_types import MList, MTuple, MDict, MSet
 from monad_std import Option
 
 
-class TestStdList(unittest.TestCase):
-    def test_index(self):
+class TestStdType(unittest.TestCase):
+    def test_list(self):
         x = MList([1, 2, 3, 4, 5])
         self.assertEqual(x.index(2), Option.some(1))
         self.assertEqual(x.index(0), Option.none())
         self.assertEqual(x.get(2), Option.some(3))
         self.assertEqual(x.get(10), Option.none())
 
-    def test_pop(self):
         x = MList([1, 2, 3, 4, 5])
         self.assertEqual(x.pop(), Option.some(5))
         self.assertEqual(x.pop(1), Option.some(2))
         x = MList()
         self.assertEqual(x.pop(), Option.none())
 
-
-class TestStdTuple(unittest.TestCase):
-    def test_index(self):
+    def test_tuple(self):
         x = MTuple((1, 3, "hey"))
         self.assertEqual(x.index("hey"), Option.some(2))
         self.assertEqual(x.index("hello"), Option.none())
         self.assertEqual(x.get(1), Option.some(3))
         self.assertEqual(x.get(5), Option.none())
 
-
-class TestStdDict(unittest.TestCase):
-    def test_index(self):
+    def test_dict(self):
         x = MDict({"a": 1, "b": 2, "c": 3})
         self.assertEqual(x.get("b"), Option.some(2))
         self.assertEqual(x.get("d"), Option.none())
 
-    def test_pop(self):
         x = MDict({"a": 1})
         self.assertEqual(x.popitem(), Option.some(("a", 1)))
         self.assertEqual(x.popitem(), Option.none())
@@ -45,9 +39,7 @@ class TestStdDict(unittest.TestCase):
         self.assertEqual(x.pop("b"), Option.none())
         self.assertEqual(x.pop("a"), Option.none())
 
-
-class TestStdSet(unittest.TestCase):
-    def test_index(self):
+    def test_set(self):
         x = MSet([1, "d"])
         p1 = x.pop()
         self.assertTrue(p1 == Option.some(1) or p1 == Option.some("d"))
