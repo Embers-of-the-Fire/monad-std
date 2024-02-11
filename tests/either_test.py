69f14f9a4466497a7c0d5fb9f4312a997da34914
Embers-of-the-Fire <stellarishs@163.com>
2024-02-11 10:39:17 +0800

    fix: add test

--- tests/either_test.py
+++ tests/either_test.py
@@ -6,6 +6,8 @@ from monad_std import UnwrapException
 
 class EitherTest(unittest.TestCase):
     def test_constructer(self):
+        self.assertEqual(Either.of_left(5), Left(5))
+        self.assertEqual(Either.of_right(5), Right(5))
         self.assertEqual(Left(5), Either.convert_either(5, True))
         self.assertEqual(Right(5), Either.convert_either(5, False))
         cvt = Either.convert_either_by(lambda x: x % 2 == 0)
@@ -17,6 +19,14 @@ class EitherTest(unittest.TestCase):
         self.assertFalse(Right(5).is_left())
         self.assertFalse(Left(5).is_right())
         self.assertTrue(Right(5).is_right())
+        self.assertNotEqual(Right(4), Left(4))
+        self.assertTrue(isinstance(Right(5), Either))
+        self.assertTrue(isinstance(Right(5), Right))
+        self.assertFalse(isinstance(Right(5), Left))
+
+    def test_dunder_methods(self):
+        self.assertEqual(hash(Left(5)), hash((True, 5)))
+        self.assertEqual(hash(Right(5)), hash((False, 5)))
 
     def test_unwrap(self):
         self.assertEqual(Left(5).unwrap_left(), 5)
